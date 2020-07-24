002b361523118fd89096f1c65cd6dac115d85305
sanyajan <sanyajan@gmail.com>
2020-07-24 06:56:57 +0300

    asda

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -2,7 +2,7 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
